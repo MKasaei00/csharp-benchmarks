e5dc3ea96e9820f4492c97254e8885859cbbb328
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2022-08-19 22:50:44 +0430

    Add Foreach benchmark

---
+++ Benchmarks/Foreach-Linq/LinqForeachBenchmark.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Foreach_Linq;
+
+public class LinqForeachBenchmark
+{
+    private const int ListSize = 1000_000;
+    private readonly List<int> list = Enumerable.Range(1, ListSize).ToList();
+
+    [Benchmark]
+    public void IterateThoroughForeach()
+    {
+        var sum = 0L;
+        foreach (var item in list)
+        {
+            sum += item;
+        }
+    }
+    
+    [Benchmark]
+    public void IterateThoroughForeachLinq()
+    {
+        var sum = 0L;
+        list.ForEach(i => sum += i);
+    }
+    
+    [Benchmark]
+    public void IterateThoroughForeachLinqAggregate()
+    {
+        var sum = list.Aggregate(0L, (current, item) => current + item);
+    }
+}
