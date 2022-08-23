39d1d4fa7f74cfe3289d95d8e1f3f2b7b0c06eb1
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2022-08-23 12:46:53 +0430

    Stackoverflow test added

---
+++ Benchmarks/StackOverFlow/Logic.cs
@@ -0,0 +1,20 @@
+namespace StackOverFlow;
+
+public class Logic
+{ public void A()
+    {
+        B();
+    }
+    private void B()
+    {
+        C();
+    }
+    private void C()
+    {
+        D();
+    }
+    private void D()
+    {
+        A();
+    }
+}
---
+++ Benchmarks/StackOverFlow/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackOverFlow
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Logic().A();
+        }
+    }
+}
